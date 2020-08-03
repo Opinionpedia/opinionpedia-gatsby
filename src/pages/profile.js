6abb8a3a437bf91bf91aab83fffdbaa89f6182aa
alexandermcnulty <alexander.mcnulty92@gmail.com>
2020-08-03 08:23:47 -0700

    add redirect to homepage when jwt is undefined

--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,123 +1,29 @@
 import React, { useState, useEffect, useReducer } from "react"
 import Layout from '../components/layout'
 import Image from '../components/image'
+import TagForm from '../components/tagForm'
 import SEO from '../components/seo'
 import { Link } from 'gatsby'
-import { Card, Elevation, FormGroup, InputGroup, Icon, TextArea, Button, Intent, Tag, ButtonGroup, Alignment } from '@blueprintjs/core'
-import { string } from "prop-types"
+import { Card, Elevation, Intent, Tag, } from '@blueprintjs/core'
 import { useCookies } from 'react-cookie'
-import jwt_decode from 'jwt-decode'
-
-const axios = require('axios');
-
-/*
-Post
-*/
-const postTag = async (id, tags, token) => {
-  let savedTags = []
-  //create Tags and Profile Tags
-  tags.forEach(async tag => {
-    //create unique Tags and Question Tags
-    try{
-      const dbTag = await 
-        axios({
-          headers: {
-            authorization: "Bearer " + token,
-          },
-          method: 'post',
-          url: 'https://opinionpedia.net/api/tag',
-          data: {
-            name: tag,
-            description: null,
-            category: null
-          }
-        })    
-      const dbQuestionTab = await 
-      axios({
-        headers: {
-          authorization: "Bearer " + token,
-        },
-        method: 'post',
-        url: 'https://opinionpedia.net/api/tag/profile',
-        data: {
-          tag_id: dbTag.data.tag_id,
-          profile_id: id,
-        }
-      })
-      console.log(dbTag.data + ", " + dbQuestionTab.data) 
-      savedTags.push(dbTag.data)
-    }
-    //create Question Tags with existing Tags
-    catch(err) {
-      if (err.response.data == "Already exists") {
-        const allTags = await 
-          axios({
-            method: 'get',
-            url: 'https://opinionpedia.net/api/tag'
-        })
-        allTags.data.forEach(async savedTag => {
-          console.log('here')
-          if(tag === savedTag.name) {
-            let newQuestionTag = await axios({
-              headers: {
-                authorization: "Bearer " + token,
-              },
-              method: 'post',
-              url: 'https://opinionpedia.net/api/tag/question',
-              data: {
-                tag_id: savedTag.id,
-                profile_id: id,              }
-            })
-            console.log("question tag added: " + newQuestionTag.data)
-            savedTags.push(savedTag.id)
-          }
-        })
-      }
-    }
-  })
-  alert('Profile tags have been updated')
-  return savedTags
-}
-
-const arrayReduce = (myArray, { type, value, setter }) => {
-  switch (type) {
-    case 'add':
-      setter("")
-      if (value == ""){
-        return [...myArray]
-      }
-      return [...myArray, value]
-    case 'remove':
-      return myArray.filter((_, index) => index !== value)
-    default:
-      return myArray
-  }
-}
 
 const ProfilePage = () => {
   const [cookies, setCookie, removeCookie] = useCookies(['account']);
-  const [username, setUsername] = useState()
+  const [username, setUsername] = useState('')
   const [description, setDescription] = useState()
   const [created, setCreated] = useState()
   const [updated, setUpdated] = useState()
 
-  const [tags, tagsDispatch] = useReducer(arrayReduce, [])
-  const [tag, setTag] = useState("")
   const [savedTags, setSavedTags] = useState([])
 
   let token = cookies['jwt']
   let id = cookies['profileId']
 
-  const addTags = (e) => {
-    let i = 1;
-    if(tags.length === 0){
-      alert("Error: Please add tags")
-      i++
+  useEffect(() => {
+    if (token === undefined) {
+      window.location.assign("https://opinionpedia.org")
     }
-    if(i == 1){
-      postTag(id, tags, token)
-    }
-  }
+  });
 
   useEffect(() => {
     fetch(`https://opinionpedia.net/api/profile/${id}`)
@@ -137,7 +43,7 @@ const ProfilePage = () => {
       setSavedTags(resultData)
       console.log(savedTags)
     })
-  }, []);
+  });
 
   return (
     <Layout>
@@ -174,7 +80,7 @@ const ProfilePage = () => {
                 <Tag
                 style={{ marginRight: '10px', marginTop: '5px', marginBottom: '5px' }}
                 large={true}
-                intent={Intent.SUCCESS}
+                intent={Intent.NONE}
                 >
                   {tag.name}
                 </Tag>
@@ -185,41 +91,7 @@ const ProfilePage = () => {
         </table>
       </Card>
       <br/>
-      <Card interactive={true} elevation={Elevation.TWO}>
-        <h2>Tags:</h2>
-        <p>Help make richer content by adding tags to your profile!</p>
-        <FormGroup
-          helperText="Describe yourself in one or two words"
-          label="Profile Tags"
-          labelFor='text-input' 
-        >
-          {tags.map((tag) => (
-            <Tag
-            style={{ marginRight: '10px', marginTop: '5px', marginBottom: '5px' }}
-            large={true}
-            intent={Intent.SUCCESS}
-            >
-              {tag}
-            </Tag>
-          ))}
-          <InputGroup 
-            id="tag" 
-            placeholder="Education"
-            onChange={e => setTag(e.target.value)}
-            value={tag}
-          />
-          <Button style={{marginTop:.2+"rem"}} onClick={() => tagsDispatch({type: 'add', value: tag, setter: setTag})}>Add Tag</Button>
-          <Button style={{marginTop:.2+"rem"}} onClick={() => tagsDispatch({type: 'remove', value: tags.length - 1, setter: setTag})}>Remove Tag</Button>
-        </FormGroup>
-        <Button 
-          style={{margin: `1.45rem 0rem`}} 
-          intent={Intent.SUCCESS}
-          large={true} 
-          fill={true} 
-          text="Submit Question"
-          onClick={addTags}
-        />
-      </Card>
+      <TagForm/>
     </Layout>
   )
 }
